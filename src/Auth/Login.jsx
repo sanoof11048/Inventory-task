075aada7f380cd26f614cbee52fc56fb1dd1499d
Muhammed Sanoof <sanoofmohammed.pvt@gmail.com>
2025-07-15 15:42:46 +0530

    navigation issue

--- src/Auth/Login.jsx
+++ src/Auth/Login.jsx
@@ -59,7 +59,7 @@ export default function Login() {
         </form>
         <p className="mt-4 text-sm text-center text-gray-600">
           Don't have an account?{' '}
-          <a href="/register" className="text-blue-600 hover:underline">
+          <a onClick={()=> navigate('/register')} className="text-blue-600 hover:underline">
             Register
           </a>
         </p>
